618b5d3048416766ae84a13328bc27f99f78427d
Fabiano Góes <fabianogoes@gmail.com>
2016-02-09 04:05:31 -0200

    commit initial

---
+++ src/main/resources/archetype-resources/src/main/java/services/ExampleService.java
@@ -0,0 +1,22 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import entities.Example;
+import repositories.ExampleRepository;
+
+@Service
+public class ExampleService {
+
+	@Autowired
+	private ExampleRepository repository;
+	
+	public Iterable<Example> list(){
+		return this.repository.findAll();
+	}
+	
+}
